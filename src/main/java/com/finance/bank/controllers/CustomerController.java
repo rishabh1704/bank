c3b38e1b498cc487bdcfd7bc8f12a3d96273a933
rishabhsharma <rishu.info99@gmail.com>
2021-08-10 01:56:40 +0530

    Added swagger descriptions.

--- src/main/java/com/finance/bank/controllers/CustomerController.java
+++ src/main/java/com/finance/bank/controllers/CustomerController.java
@@ -4,6 +4,8 @@ import com.finance.bank.dto.CashTransactionDTO;
 import com.finance.bank.dto.CustomerDTO;
 import com.finance.bank.dto.TransactionDTO;
 import com.finance.bank.services.CustomerService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +19,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("customers")
-//@Api(tags = { "Accounts and Transactions REST endpoints" })
+@Api(tags = {"Functions related to customer operations"})
 public class CustomerController {
 
     private CustomerService customerService;
@@ -26,43 +28,50 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
-    @RequestMapping("/home")
+    @GetMapping("/home")
     public String toHome() {
         return "Welcome to the banking application.";
     }
 
+    @ApiOperation(value = "Check balance of an account")
     @PostMapping("/balance")
     public Double checkBalance(@RequestBody Long accountId) {
         return this.customerService.balanceEnquiry(accountId);
     }
 
+    @ApiOperation(value = "Check balance of an account")
     @GetMapping("/history")
     public List<TransactionDTO> getTransactionHistory(@RequestBody Long accountId) {
         return this.customerService.transactionHistory(accountId);
     }
 
+    @ApiOperation(value = "Transfer money from one account to another")
     @PostMapping("/transfer_money")
     public ResponseEntity<Object> transferMoney(@RequestBody TransactionDTO transfer) {
         String msg = this.customerService.transferMoney(transfer);
         return ResponseEntity.status(HttpStatus.OK).body(msg);
     }
 
+    @ApiOperation(value = "View Profile info of a customer")
     @GetMapping("/profile_info")
     public CustomerDTO viewProfile(@RequestBody Long customerId) {
         return this.customerService.viewPersonalDetails(customerId);
     }
 
+    @ApiOperation(value = "Get Unread notifications")
     @GetMapping("/notifications")
     public String getNotifications(@RequestBody Long customerId) {
         return this.customerService.getNotifications(customerId);
     }
 
+    @ApiOperation(value = "Add cash/funds to the account")
     @PostMapping("/add_money")
     public ResponseEntity<Object> addMoney(@RequestBody CashTransactionDTO data) {
         String msg = this.customerService.addFunds(data);
         return ResponseEntity.status(HttpStatus.OK).body(msg);
     }
 
+    @ApiOperation(value = "Withdraw cash/funds from the account")
     @PostMapping("/withdraw_money")
     public ResponseEntity<Object> withdrawMoney(@RequestBody CashTransactionDTO data) {
         String msg = this.customerService.withdrawFunds(data);
--- src/main/java/com/finance/bank/controllers/EmployeeController.java
+++ src/main/java/com/finance/bank/controllers/EmployeeController.java
@@ -3,12 +3,15 @@ package com.finance.bank.controllers;
 import com.finance.bank.dto.CustomerDTO;
 import com.finance.bank.dto.TransientInfo;
 import com.finance.bank.services.EmployeeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("employees")
+@Api(tags = {"Functions related to employee operations"})
 public class EmployeeController {
 
     private EmployeeService employeeService;
@@ -17,35 +20,40 @@ public class EmployeeController {
         this.employeeService = employeeService;
     }
 
-    @RequestMapping("/home")
+    @GetMapping("/home")
     public String toHome() {
         return "Welcome to the employee portal.";
     }
 
+    @ApiOperation(value = "create a new customer with default account")
     @PostMapping("/new_customer")
     public ResponseEntity<Object> createNewCustomer(@RequestBody CustomerDTO data) {
         Long id = this.employeeService.createCustomer(data);
         return ResponseEntity.status(HttpStatus.OK).body("Your Customer id is : " + id.toString());
     }
 
+    @ApiOperation(value = "update the customer information")
     @PostMapping("/update_customer/{id}")
     public ResponseEntity<Object> updateCustomer(@PathVariable(name = "id") Long id, @RequestBody CustomerDTO data) {
         String msg = this.employeeService.updateCustomer(id, data);
         return ResponseEntity.status(HttpStatus.OK).body(msg);
     }
 
+    @ApiOperation(value = "Delete a customer")
     @DeleteMapping("/delete_customer/{id}")
     public ResponseEntity<Object> deleteCustomer(@PathVariable(name = "id") Long id) {
         String message = this.employeeService.deleteCustomer(id);
         return ResponseEntity.status(HttpStatus.OK).body(message);
     }
 
+    @ApiOperation(value = "create a new account for a customer")
     @GetMapping("/create_account/{id}")
     public ResponseEntity<Object> createAccount(@PathVariable(name = "id") Long customerId) {
         Long id = this.employeeService.createNewAccount(customerId);
         return ResponseEntity.status(HttpStatus.OK).body("Account created with id : " + id.toString());
     }
 
+    @ApiOperation(value = "delete account for a customer along with all its transactions")
     @PostMapping("/delete_account")
     public ResponseEntity<Object> deleteAccount(@RequestBody TransientInfo data) {
         String msg = this.employeeService.deleteAccount(data.getCustomerId(), data.getAccountId());

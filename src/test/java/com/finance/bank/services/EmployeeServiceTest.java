18443aee4526a6cd58c8d95fa160fdece8ed36b2
rishabhsharma <rishu.info99@gmail.com>
2021-08-16 14:33:22 +0530

    Added more test cases.

--- src/test/java/com/finance/bank/services/EmployeeServiceTest.java
+++ src/test/java/com/finance/bank/services/EmployeeServiceTest.java
@@ -1,15 +1,18 @@
 package com.finance.bank.services;
 
+import com.finance.bank.dto.CashTransactionDTO;
 import com.finance.bank.dto.CustomerDTO;
-import com.finance.bank.model.Address;
-import com.finance.bank.model.Contact;
-import com.finance.bank.model.Customer;
+import com.finance.bank.dto.TransactionDTO;
+import com.finance.bank.model.*;
+import com.finance.bank.repositories.AccountRepository;
 import com.finance.bank.repositories.CustomerRepository;
+import com.finance.bank.repositories.TransactionRepository;
 import lombok.extern.log4j.Log4j2;
 import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,11 +29,17 @@ class EmployeeServiceTest {
     @MockBean
     private CustomerRepository customerRepository;
 
-    private Customer setUp() {
+    @MockBean
+    private TransactionRepository transactionRepository;
+
+    @MockBean
+    private AccountRepository accountRepository;
+
+    private Customer setUp(Long id) {
         Customer customer = new Customer();
         customer.setFirstName("Rishabh");
         customer.setLastName("Sharma");
-        customer.setId(14L);
+        customer.setId(id);
 
         Address address = new Address();
         address.setPinCode("987897"); address.setState("Delhi"); address.setCity("new Delhi"); address.setLine1("what"); address.setLine2("Line2");
@@ -43,10 +52,21 @@ class EmployeeServiceTest {
         return customer;
     }
 
+    private Account setUpAccount(Customer customer, Long id) {
+        Account account = new Account();
+        account.setOwner(customer);
+        account.setBalance(10000.0);
+        account.setId(id);
+        account.setAccountNumber("o878uy");
+        account.setAccountStatus(AccountType.SAVINGS.toString());
+
+        return account;
+    }
+
     @Test
     @DisplayName("Find customer by id")
     public void testFindById() {
-        Customer customer = setUp();
+        Customer customer = setUp(14L);
 
 //        called from mock repo directly
         Mockito.doReturn(customer).when(customerRepository).findCustomerById(14L);
@@ -64,7 +84,7 @@ class EmployeeServiceTest {
     @Test
     @DisplayName("Check if null on non id query")
     public void testNullById() {
-        Customer customer = setUp();
+        Customer customer = setUp(14L);
         Mockito.doReturn(customer).when(customerRepository).findCustomerById(14L);
 
         ModelMapper mapper = new ModelMapper();
@@ -74,6 +94,66 @@ class EmployeeServiceTest {
             returnedCustomer = mapper.map(customerDTO, Customer.class);
 
         Assertions.assertNull(returnedCustomer.getFirstName(), "Object found surprisingly.");
+    }
+
+    @Test
+    @DisplayName("Transfer Money")
+    public void testTransferMoney() {
+        Customer customer = setUp(14l);
+        Account acc1 = setUpAccount(customer, 20l);
+        Account acc2 = setUpAccount(customer, 21l);
+
+        TransactionDTO transactionDTO = new TransactionDTO();
+        transactionDTO.setFrom(20l);
+        transactionDTO.setTo(21l);
+        transactionDTO.setAmount(100.0);
+
+        Mockito.doReturn(acc1).when(accountRepository).findAccountById(20L);
+        Mockito.doReturn(acc2).when(accountRepository).findAccountById(21L);
+
+//        call from service
+        String message = customerService.transferMoney(transactionDTO);
+
+        Assertions.assertEquals(accountRepository.findAccountById(20L).getBalance(), 9900.00);
+        Assertions.assertEquals(accountRepository.findAccountById(21L).getBalance(), 10100.00);
+    }
+
+    @Test
+    @DisplayName("Add funds")
+    public void testAddFunds() {
+        Customer customer = setUp(14l);
+        Account account = setUpAccount(customer, 11l);
+
+        CashTransactionDTO cashTransactionDTO = new CashTransactionDTO();
+        cashTransactionDTO.setAmount(1200.0);
+        cashTransactionDTO.setAccountId(11l);
+        cashTransactionDTO.setCustomerId(14l);
+
+        Mockito.doReturn(customer).when(customerRepository).findCustomerById(14l);
+        Mockito.doReturn(account).when(accountRepository).findAccountById(11l);
+
+        String message = customerService.addFunds(cashTransactionDTO);
+
+        Assertions.assertEquals(accountRepository.findAccountById(11l).getBalance(), 10000.0 + 1200.0);
+    }
+
+    @Test
+    @DisplayName("Withdraw Funds")
+    public void testWithdrawFunds() {
+        Customer customer = setUp(14l);
+        Account account = setUpAccount(customer, 11l);
+
+        CashTransactionDTO cashTransactionDTO = new CashTransactionDTO();
+        cashTransactionDTO.setAmount(1200.0);
+        cashTransactionDTO.setAccountId(11l);
+        cashTransactionDTO.setCustomerId(14l);
+
+        Mockito.doReturn(customer).when(customerRepository).findCustomerById(14l);
+        Mockito.doReturn(account).when(accountRepository).findAccountById(11l);
+
+        String message = customerService.withdrawFunds(cashTransactionDTO);
+
+        Assertions.assertEquals(accountRepository.findAccountById(11l).getBalance(), 10000.0 - 1200.0);
     }
 
 }
